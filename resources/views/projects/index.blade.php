000acb679ea354c685016b867fe6ef4d3fc7cb8a
paulo.carvalho <paulo.carvalho@wellowgroup.com>
2023-07-31 17:52:59 +0100

    Episode 4

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>Birdboard</h1>
+
+        <ul>
+            @forelse ($projects as $project)
+                <li>
+                    <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                </li>
+            @empty
+            <li>
+                No projects found
+            </li>
+            @endforelse
+        </ul>
+    </body>
+</html>
---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Project;
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+{
+    /** @test */
+    public function it_has_a_path()
+    {
+        $this->withoutExceptionHandling();
+        $project = Project::factory()->create();
+
+
+       $this->assertEquals('/projects/' . $project->id, $project->path());
+    }
+}
